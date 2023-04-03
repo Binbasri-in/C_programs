37665a7dc56df8f8dc0f25a534d34beccf6efdb7
Mohammed Ali Alsakkaf (Binbasri) <87440507+Binbasri-in@users.noreply.github.com>
2023-04-03 12:42:41 +0530

    Update 6_double_queues.c

--- BMSIT/lab_programs_21CS38A/6_double_queues.c
+++ BMSIT/lab_programs_21CS38A/6_double_queues.c
@@ -58,3 +58,113 @@ void delete_front(int *f, int *r, int q[])
         *r = -1;
     }
 }
+
+void delete_rear(int *f, int *r, int q[])
+{
+    int item;
+
+    // check underflow condition
+    if(*f > *r)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+
+    // delete the element
+    printf("Item deleted is: %d\n",q[*r]);
+    *r = *r - 1;
+
+    // if reached end of queue, reset the queue
+    if(*f > *r)
+    {
+        *f = 0;
+        *r = -1;
+    }
+}
+
+void display(int *f, int *r, int q[])
+{
+    int i;
+
+    // check underflow condition
+    if(*f > *r)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+
+    // display the queue
+    printf("Queue is: ");
+    for(i = *f; i <= *r; i++)
+    {
+        printf("%d ",q[i]);
+    }
+    printf("\n");
+}
+
+void main()
+{
+    int f, r, q[queue_size], item, choice;
+
+    // initialize the queue
+    f = 0;
+    r = -1;
+
+    for(;;)
+    {
+        printf("1. Insert front\n");
+        printf("2. Insert rear\n");
+        printf("3. Delete front\n");
+        printf("4. Delete rear\n");
+        printf("5. Display\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+            case 1: printf("Enter the item to insert: ");
+                    scanf("%d",&item);
+                    insert_front(item, &f, &r, q);
+                    break;
+            case 2: printf("Enter the item to insert: ");
+                    scanf("%d",&item);
+                    insert_rear(item, &f, &r, q);
+                    break;
+            case 3: delete_front(&f, &r, q);
+                    break;
+            case 4: delete_rear(&f, &r, q);
+                    break;
+            case 5: display(&f, &r, q);
+                    break;
+            case 6: exit(0);
+        }
+    }
+}
+
+
+// output
+// 1. Insert front
+// 2. Insert rear
+// 3. Delete front
+// 4. Delete rear
+// 5. Display
+// 6. Exit
+// Enter your choice: 1
+// Enter the item to insert: 10
+// 1. Insert front
+// 2. Insert rear
+// 3. Delete front
+// 4. Delete rear
+// 5. Display
+// 6. Exit
+// Enter your choice: 1
+// Enter the item to insert: 20
+// 1. Insert front
+// 2. Insert rear
+// 3. Delete front
+// 4. Delete rear
+// 5. Display
+// 6. Exit
+// Enter your choice: 5
+// Queue is: 20 10
