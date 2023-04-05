f70e6f6786bb6aba150b574f66f691f433bc0441
Mohammed Ali Alsakkaf (Binbasri) <87440507+Binbasri-in@users.noreply.github.com>
2023-04-06 01:20:56 +0530

    Update 4_evaluate_suffix.c

--- BMSIT/lab_programs_21CS38A/4_evaluate_suffix.c
+++ BMSIT/lab_programs_21CS38A/4_evaluate_suffix.c
@@ -1,109 +1,60 @@
 // 4. a program to evaluate a postfix expression using stack
-#include<stdio.h>
-#include<math.h>
-#define stack_size 100
+// 4. evaluation of postfix expressions
 
-// prototype of the functions
-void push(int item, int *top,int s[]);
-int pop(int *top,int s[]);
-int evaluate_postfix(char postfix[]);
+#include <stdio.h>
+#include <math.h> // remember to add -lm in the compilation (gcc file.c -lm)
+#define MAX 20
 
-int main()
+//prototypes
+int eval_post(char exp[]);
+
+void main()
 {
-    // initialize the string to store the postfix expression
-    char postfix[stack_size];
-    // get the postfix expression
-    printf("Enter the postfix expression: ");
-    scanf("%s",postfix);
-    // evaluate the postfix expression
-    printf("The value of the postfix expression is: %d",evaluate_postfix(postfix));
-    return 0;
+    char postfix[MAX];
+
+    printf("Enter postfix exp (No spaces and single digit operands): ");
+    scanf("%s", postfix);
+    printf("The result: %d\n", eval_post(postfix));
 }
 
-int evaluate_postfix(char postfix[])
+int eval_post(char exp[])
 {
-    // initialize the stack
-    int stack[stack_size],top=-1;
-    // initialize the variables to store the operands
-    int operand1,operand2, symbol;
-    // initialize the variable to store the result
-    int result;
-    // iterate through the postfix expression
-    for (int i = 0; postfix[i]!='\0'; i++)
+    int s[MAX], top = -1, i, op1, op2;
+    char symbol;
+
+    for (i = 0; exp[i] != '\0'; i++)
     {
-        // get the symbol from the postfix expression
-        symbol=postfix[i];
-        // evaluate the expression if operator or store it if operand
+        symbol = exp[i];
+
         switch(symbol)
         {
-            case '+':
-                operand2=pop(&top,stack);
-                operand1=pop(&top,stack);
-                result=operand1+operand2;
-                push(result,&top,stack);
-                break;
-            case '-':
-                operand2=pop(&top,stack);
-                operand1=pop(&top,stack);
-                result=operand1-operand2;
-                push(result,&top,stack);
-                break;
-            case '*':
-                operand2=pop(&top,stack);
-                operand1=pop(&top,stack);
-                result=operand1*operand2;
-                push(result,&top,stack);
-                break;
-            case '/':
-                operand2=pop(&top,stack);
-                operand1=pop(&top,stack);
-                result=operand1/operand2;
-                push(result,&top,stack);
-                break;
-            case '^':
-                operand2=pop(&top,stack);
-                operand1=pop(&top,stack);
-                result=(int)pow((double)operand1,(double)operand2);
-                push(result,&top,stack);
-                break;
-            default:
-                // convert the character to integer and store it in the stack
-                push(symbol-'0',&top,stack);
-                break;
+            case '+': op2 = s[top--];
+                      op1 = s[top--];
+                      s[++top] = op1 + op2;
+                      break;
+            case '-': op2 = s[top--];
+                      op1 = s[top--];
+                      s[++top] = op1 - op2;
+                      break;
+            case '*': op2 = s[top--];
+                      op1 = s[top--];
+                      s[++top] = op1 * op2;
+                      break;
+            case '/': op2 = s[top--];
+                      op1 = s[top--];
+                      s[++top] = op1 / op2;
+                      break;
+            case '%': op2 = s[top--];
+                      op1 = s[top--];
+                      s[++top] = op1 % op2;
+                      break;
+            case '^': op2 = s[top--];
+                      op1 = s[top--];
+                      s[++top] = (int)pow((double)op1,(double)op2);
+                      break;
+            default: s[++top] = symbol - '0';
         }
     }
-    return pop(&top,stack);
-}
 
-void push(int item, int *top,int s[])
-{
-    // check if the stack is full
-    if(*top==stack_size-1)
-        printf("Stack overflow");
-    else
-    {
-        // increment the top of the stack
-        *top=*top+1;
-        // add the item to the stack
-        s[*top]=item;
-    }
-}
-
-int pop(int *top,int s[])
-{
-    // check if the stack is empty
-    if(*top==-1)
-    {
-        printf("Stack underflow");
-        return -1;
-    }
-    else
-    {
-        // get the item from the stack
-        int item=s[*top];
-        // decrement the top of the stack
-        *top=*top-1;
-        // return the item
-        return item;
-    }
+    return s[top];
 }
