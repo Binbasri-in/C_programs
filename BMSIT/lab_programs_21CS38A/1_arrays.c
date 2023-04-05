af52ae93ad3e2976bc48d14ee392f5db8aca22eb
Mohammed Ali Alsakkaf (Binbasri) <87440507+Binbasri-in@users.noreply.github.com>
2023-04-06 00:11:51 +0530

    Update 1_arrays.c

--- BMSIT/lab_programs_21CS38A/1_arrays.c
+++ BMSIT/lab_programs_21CS38A/1_arrays.c
@@ -10,113 +10,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *a;        // Pointer to hold the memory alocated for array
-int arraysize; // Maximum number of elements that the array can hold
-int n = 0;     // number of elements in the array at any given instant
+// global
+int *a = NULL;
+int max_size = 100;
+int n = 0;
+
+// prototypes
+void create_array();
+void display_array();
+void insert_to(int item, int pos);
+void delete_from(int pos);
+
+void main()
+{
+    int ch, item, pos;
+
+    printf("Enter number of items to insert initially: ");
+    scanf("%d", &n);
+
+    create_array();
+
+    for(;;)
+    {
+        printf("1.display  2.insert  3.delete  4.exit\n");
+        scanf("%d", &ch);
+
+        switch(ch)
+        {
+            case 1: display_array();
+                    break;
+            case 2: printf("Enter item to insert: ");
+                    scanf("%d", &item);
+                    printf("Enter position(start from 0): ");
+                    scanf("%d", &pos);
+                    insert_to(item, pos);
+                    break;
+            case 3: printf("Enter position to delete(start from 0): ");
+                    scanf("%d", &pos);
+                    delete_from(pos);
+                    break;
+            case 4: exit(0);
+                    break;
+        }
+    }
+}
 
 void create_array()
 {
     int i;
-    a = (int *)malloc(arraysize * sizeof(int));
-    
-    printf("Enter %d elements\n", n); // n is the current no of elements
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
+
+    a = malloc(max_size*sizeof(int)); // allocate memory for the array
+
+    printf("Enter %d Element to insert to array\n", n); // insert n elements to the array
+    for(i = 0; i < n; i++)
+        scanf("%d", (a+i));
+
 }
+
 
 void display_array()
 {
     int i;
-    if(n == 0)
-    {
-        printf("No elements in the array\n");
-        return;
-    }
-    printf("The array elements are\n");
-    for (i = 0; i < n; i++)
-    {
+
+    printf("The array is: "); // print out each elemnt from the array
+    for(i = 0; i < n; i++)
         printf("%d\t", a[i]);
-    }
+    
+    printf("\n");
 }
 
-void insert_array(int item, int pos)
+void insert_to(int item, int pos)
 {
     int i;
-    if (n == arraysize)
+
+    if (pos > n || pos < 0) // check the position
     {
-        printf("Array is full. Cannot insert \n");
+        printf("Invalid position\n");
         return;
     }
-    if (pos > n || pos < 0)
-    {
-        printf("Invalid position \n");
-        return;
-    }
+
+    // free space at the position by moving each one after it one step to the right
     for (i = n - 1; i >= pos; i--)
-    {
-        a[i + 1] = a[i];
-    }
-    a[pos] = item;
-    n = n + 1;
+        a[i+1] = a[i];
+
+    a[pos] = item; // insert the item into the position
+    n = n + 1; // increment the elements counter
 }
 
-void delete_array(int pos)
+
+void delete_from(int pos)
 {
     int i;
-    if (n == 0)
-    {
-        printf("No elements in the array, Cannot delete\n");
-        return;
-    }
-    if (pos >= n || pos < 0)
-    {
-        printf("Invalid position \n");
-        return;
-    }
-    printf("Item deleted= %d \n", a[pos]);
-    for (i = pos + 1; i < n; i++)
-    {
-        a[i - 1] = a[i];
-    }
-    n = n - 1;
-}
 
-int main()
-{
-    int item, pos, choice;
-    printf("To Create an Array\n");
-    printf("Enter the maximum size of array\n");
-    scanf("%d", &arraysize);
-    printf("Enter the number of elements to be read initially \n");
-    scanf("%d", &n);
-    create_array();
-    for (;;)
+    if (pos > n || pos < 0) // check the position
     {
-        printf("l.Display 2.Insert 3.Delete 4.Exit\n");
-        printf("Enter your choice\n");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            display_array();
-            break;
-        case 2:
-            printf("Enter the item to insert\n");
-            scanf("%d", &item);
-            printf("Enter the index position to insert\n");
-            scanf("%d", &pos);
-            insert_array(item, pos);
-            break;
-        case 3:
-            printf("Enter the index position to delete\n");
-            scanf("%d", &pos);
-            delete_array(pos);
-            break;
-        case 4:
-            free(a);
-            exit(0);
-        }
+        printf("Invalid position\n");
+        return;
     }
+
+    printf("item deleted is: %d\n", a[pos]);// print item deleted
+
+    // move each element one step to the left to delete the item
+    for (i = pos + 1; i < n; i++)
+        a[i-1] = a[i];
+
+    n = n - 1; // decrement the elements counter
 }
