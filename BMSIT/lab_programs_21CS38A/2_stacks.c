9d6dfd47a6a79e44de2acfeb4d74c95860e54bee
Mohammed Ali Alsakkaf (Binbasri) <87440507+Binbasri-in@users.noreply.github.com>
2023-04-06 00:31:11 +0530

    Update 2_stacks.c

--- BMSIT/lab_programs_21CS38A/2_stacks.c
+++ BMSIT/lab_programs_21CS38A/2_stacks.c
@@ -1,149 +1,96 @@
 // 2. a program to implement a stack using array
-#include<stdio.h>
-#include<stdlib.h>
-#define stack_size 100
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 20
 
-// prototype of the functions
-void push(int item, int *top,int s[]);
-void pop(int *top,int s[]);
-void display(int *top,int s[]);
-void is_empty(int *top);
-void is_full(int *top);
+// global
+int s[MAX];
+int top = -1;
 
-int main()
+// prototypes
+void push(int item);
+void pop();
+void is_overflow_or_underflow();
+void display();
+
+void main()
 {
-    // initialize the stack
-    int stack[stack_size],top=-1;
-    // initialize the variable to store the choice
-    int choice;
-    // initialize the variable to store the item
-    int item;
-    // iterate through the menu
-    while(1)
+    int item, ch;
+
+    for(;;)
     {
-        // display the menu
-        printf("1. Push an element to the stack\n");
-        printf("2. Pop an element from the stack\n");
-        printf("3. Display the stack\n");
-        printf("4. Check if the stack is empty\n");
-        printf("5. Check if the stack is full\n");
-        printf("6. Exit\n");
-        // get the choice
+        printf("1.push  2.pop  3.check status  4. display  5.exit\n");
         printf("Enter your choice: ");
-        scanf("%d",&choice);
-        // perform the operation based on the choice
-        switch(choice)
+        scanf("%d", &ch);
+
+        switch(ch)
         {
-            case 1:
-                // get the item
-                printf("Enter the item: ");
-                scanf("%d",&item);
-                // push the item to the stack
-                push(item,&top,stack);
-                break;
-            case 2:
-                // pop the item from the stack
-                pop(&top,stack);
-                break;
-            case 3:
-                // display the stack
-                display(&top,stack);
-                break;
-            case 4:
-                // check if the stack is empty
-                is_empty(&top);
-                break;
-            case 5:
-                // check if the stack is full
-                is_full(&top);
-                break;
-            case 6:
-                // exit the program
-                exit(0);
-            default:
-                // display the error message
-                printf("Invalid choice\n");
+            case 1: printf("Enter item to push: ");
+                    scanf("%d", &item);
+                    push(item);
+                    break;
+            case 2: pop();
+                    break;
+            case 3: is_overflow_or_underflow();
+                    break;
+            case 4: display();
+                    break;
+            case 5: exit(0);
         }
     }
-    return 0;
 }
 
-void push(int item, int *top,int s[])
+void push(int item)
 {
-    // check if the stack is full
-    if(*top==stack_size-1)
+    if (top == MAX-1) // check overflow condition
     {
-        // display the error message
-        printf("Stack overflow\n");
+        printf("Stack Overflow\n");
         return;
     }
-    // increment the top
-    (*top)++;
-    // push the item to the stack
-    s[*top]=item;
+
+    top = top + 1; // push to the stack
+    s[top] = item;
 }
 
-void pop(int *top,int s[])
+void pop()
 {
-    // check if the stack is empty
-    if(*top==-1)
+    if (top == -1) // check underflow condition
     {
-        // display the error message
-        printf("Stack underflow\n");
+        printf("Stack Underflow\n");
         return;
     }
-    // display the popped item
-    printf("The popped item is: %d\n",s[*top]);
-    // decrement the top
-    (*top)--;
+
+    
+    printf("Poped: %d\n", s[top]); // print the poped item
+    top = top - 1; // pop it
 }
 
-void display(int *top,int s[])
+void is_overflow_or_underflow()
 {
-    // check if the stack is empty
-    if(*top==-1)
+    if (top == -1)
+        printf("Stack Underflow\n");
+    else if (top == MAX-1)
+        printf("Stack Overflow\n");
+    else
+        printf("Not Underflow nor Overflow\n");
+}
+
+void display()
+{
+    int i;
+
+    if (top == -1) // check underflow condition
     {
-        // display the error message
         printf("Stack is empty\n");
         return;
     }
-    // display the stack
+
     printf("The stack is: ");
-    for (int i = *top; i >=0; i--)
-    {
-        printf("%d ",s[i]);
-    }
+    for (i = 0; i <= top; i++)
+        printf("%d\t", s[i]);
     printf("\n");
+
 }
 
-void is_empty(int *top)
-{
-    // check if the stack is empty
-    if(*top==-1)
-    {
-        // display the message
-        printf("Stack is empty\n");
-    }
-    else
-    {
-        // display the message
-        printf("Stack is not empty\n");
-    }
-}
-
-void is_full(int *top)
-{
-    // check if the stack is full
-    if(*top==stack_size-1)
-    {
-        // display the message
-        printf("Stack is full\n");
-    }
-    else
-    {
-        // display the message
-        printf("Stack is not full\n");
-    }
-}
 
 
