29d8189bbc573272fc2d15323e4940aa61faa9ba
Mohammed Ali Alsakkaf (Binbasri) <87440507+Binbasri-in@users.noreply.github.com>
2023-04-06 09:48:10 +0530

    Update 6_double_queues.c

--- BMSIT/lab_programs_21CS38A/6_double_queues.c
+++ BMSIT/lab_programs_21CS38A/6_double_queues.c
@@ -1,123 +1,29 @@
 // double ended queues
+
+// 6. de-queue
+
 #include <stdio.h>
-#define queue_size 50
+#include <stdlib.h>
+#define MAX 20
 
-void insert_front(int item, int *f, int *r, int q[])
-{
-    // check initial condition
-    if(*f == 0 && *r != -1)
-    {
-        printf("Insert front is not possible\n");
-        return;
-    }
-    if(*f == 0)
-    {
-        *r = *r + 1;
-        q[*r] = item;
-        return;
-    }
+// global
+int q[MAX];
+int r = -1, f = 0;
 
-    // insert the element
-    *f = *f - 1;
-    q[*f] = item;
-}
-
-void insert_rear(int item, int *f, int *r, int q[])
-{
-    // check overflow condition
-    if(*r == queue_size - 1)
-    {
-        printf("Queue Overflow\n");
-        return;
-    }
-
-    // insert the element
-    *r = *r + 1;
-    q[*r] = item;
-}
-
-void delete_front(int *f, int *r, int q[])
-{
-    int item;
-
-    // check underflow condition
-    if(*f > *r)
-    {
-        printf("Queue Underflow\n");
-        return;
-    }
-
-    // delete the element
-    printf("Item deleted is: %d\n",q[*f]);
-    *f = *f + 1;
-
-    // if reached end of queue, reset the queue
-    if(*f > *r)
-    {
-        *f = 0;
-        *r = -1;
-    }
-}
-
-void delete_rear(int *f, int *r, int q[])
-{
-    int item;
-
-    // check underflow condition
-    if(*f > *r)
-    {
-        printf("Queue Underflow\n");
-        return;
-    }
-
-    // delete the element
-    printf("Item deleted is: %d\n",q[*r]);
-    *r = *r - 1;
-
-    // if reached end of queue, reset the queue
-    if(*f > *r)
-    {
-        *f = 0;
-        *r = -1;
-    }
-}
-
-void display(int *f, int *r, int q[])
-{
-    int i;
-
-    // check underflow condition
-    if(*f > *r)
-    {
-        printf("Queue Underflow\n");
-        return;
-    }
-
-    // display the queue
-    printf("Queue is: ");
-    for(i = *f; i <= *r; i++)
-    {
-        printf("%d ",q[i]);
-    }
-    printf("\n");
-}
+// prototypes
+void insert_rear(int item);
+void insert_front(int item);
+void delete_front();
+void delete_rear();
+void display();
 
 void main()
 {
-    int f, r, q[queue_size], item, choice;
-
-    // initialize the queue
-    f = 0;
-    r = -1;
+    int item, choice;
 
     for(;;)
     {
-        printf("1. Insert front\n");
-        printf("2. Insert rear\n");
-        printf("3. Delete front\n");
-        printf("4. Delete rear\n");
-        printf("5. Display\n");
-        printf("6. Exit\n");
+        printf("1. Insert front  2. Insert rear  3. Delete front  4. Delete rear  5. Display  6. Exit\n");
         printf("Enter your choice: ");
         scanf("%d",&choice);
 
@@ -125,21 +31,81 @@ void main()
         {
             case 1: printf("Enter the item to insert: ");
                     scanf("%d",&item);
-                    insert_front(item, &f, &r, q);
+                    insert_front(item);
                     break;
             case 2: printf("Enter the item to insert: ");
                     scanf("%d",&item);
-                    insert_rear(item, &f, &r, q);
+                    insert_rear(item);
                     break;
-            case 3: delete_front(&f, &r, q);
+            case 3: delete_front();
                     break;
-            case 4: delete_rear(&f, &r, q);
+            case 4: delete_rear();
                     break;
-            case 5: display(&f, &r, q);
+            case 5: display();
                     break;
             case 6: exit(0);
         }
     }
+}
+
+void insert_rear(int item)
+{
+    if(r == MAX - 1)
+    {
+        printf("Queue overflow\n");
+        return;
+    }
+    q[++r] = item;
+}
+
+void insert_front(int item)
+{
+    if(f == 0 && r != -1)
+    {
+        printf("Queue overflow front\n");
+        return;
+    }
+    if(f == 0)
+    {
+        q[++r] = item;
+        return;
+    }
+    q[--f] = item;
+}
+
+void delete_front()
+{
+    if(f > r)
+    {
+        printf("Queue underflow\n");
+        return;
+    }
+    f++;
+}
+
+void delete_rear()
+{
+    if(f > r)
+    {
+        printf("Queue underflow\n");
+        return;
+    }
+    r--;
+}
+
+void display()
+{
+    int i;
+
+    if(f > r)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue is: ");
+    for(i = f; i <= r; i++)
+        printf("%d ",q[i]);
+    printf("\n");
 }
 
 
